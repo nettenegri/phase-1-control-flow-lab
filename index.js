7c1b8c73264f6ca21e081dbba35c2dc2c9ff4537
nettenegri <nettenegri@gmail.com>
2022-11-22 19:36:48 -0700

    commit tree 4d8043199d33dd50502aebd4b20f3326276b203b

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let response;
+  if (distance <= 400) {
+    response = "This one is on me!";
+  } else if (distance > 2500) {
+    response = "No can do.";
+  } else if (distance < 2000) {
+    response = "That will be twenty bucks.";
+  } else {
+    response = "I will gladly take your thirty bucks.";
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
